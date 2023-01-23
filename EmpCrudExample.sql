cb6661373db4af05d89b18e6e5cc1c6a64466b55
Ashwinijagtap24 <119514905+Ashwinijagtap24@users.noreply.github.com>
2023-01-23 09:45:43 +0530

    Add files via upload

---
+++ EmpCrudExample.sql
@@ -0,0 +1,11 @@
+create table EmployeeCrudTb
+(
+Empid int primary key identity(1,1),
+EmpName varchar(50),
+DeptName varchar(50),
+Salary int,
+Age int
+)
+--drop table EmployeeCrud
+select * from EmployeeCrudTb
+Select * from Dept1
